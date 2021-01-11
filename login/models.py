2b47134ebc1746a37db18fd7517c3c4a76d27056
keremgocen <keremgocen@gmail.com>
2021-01-12 01:39:15 +0300

    add MinLengthValidator for customer model

--- login/models.py
+++ login/models.py
@@ -1,7 +1,8 @@
+from django.core.validators import MinLengthValidator
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class Customer(AbstractUser):
     pin_code = models.CharField(
-        max_length=5, default='00000', editable=True, unique=True)
+        max_length=5, default='00000', editable=True, unique=True, validators=[MinLengthValidator(5)])
