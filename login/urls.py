ef6e44b8cf68b50d90e0cf495072f46d0a0c737c
keremgocen <keremgocen@gmail.com>
2021-01-12 02:25:20 +0300

    clean up

--- login/urls.py
+++ login/urls.py
@@ -1,23 +1,14 @@
 from django.urls import include, path
 
 from . import views
-# from django.conf.urls import patterns, url
 from rest_framework import routers
-# from rest_framework.urlpatterns import format_suffix_patterns
 
 router = routers.DefaultRouter()
-router.register(r'customers', views.CustomerViewSet)
+router.register(r"customers", views.CustomerViewSet)
+
+# todo display customer_login in api-root
 
 urlpatterns = [
-    path('', include(router.urls)),
-    # path('customers_list/', views.customer_list),
-    # path('customers/<int:pk>/', views.customer_detail),
-    # path('api/customers/', views.customer_list),
-    path('api/customer_login/', views.customer_list)
+    path("", include(router.urls)),
+    path("api/customer_login/", views.customer_login),
 ]
-# url(r'^api/v1/customers/(?P<slug>[0-9]+)$', 'customer_detail')
-
-# urlpatterns = format_suffix_patterns(urlpatterns)
-# urlpatterns += [
-#     path('api-auth/', include('rest_framework.urls')),
-# ]
--- login/views.py
+++ login/views.py
@@ -1,110 +1,43 @@
-# from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from rest_framework import viewsets
 from rest_framework import permissions
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
-from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
+
 from login.models import Customer
 from login.serializers import CustomerSerializer
-# from rest_framework.mixins import (
-#     CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin
-# )
-# from rest_framework.viewsets import GenericViewSet
 
 
 @csrf_exempt
-@api_view(['GET', 'POST'])
-def customer_list(request, format=None):
+@api_view(["POST"])
+def customer_login(request, format=None):
     """
-    List all customers, or create a new customer.
+    Validates customer pin_code.
     """
-    if request.method == 'GET':
-        customer = Customer.objects.all()
-        serializer = CustomerSerializer(customers, many=True)
-        return Response(serializer.data)
+    pin_code = request.POST.get("pin_code", "")
+    if len(pin_code) != 5:
+        # todo validation response about pin length
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'POST':
-        print('kerem', request.POST)
-        print('kerem', request.data.get('pin_code'))
-        try:
-            customer = Customer.objects.get(
-                pin_code=request.data.get('pin_code'))
-        except (KeyError, Customer.DoesNotExist):
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        serializer = CustomerSerializer(customer, data=request.data)
-        if serializer.is_valid():
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@csrf_exempt
-@api_view(['GET', 'PUT', 'DELETE'])
-def customer_detail(request, pk):
-    """
-    Retrieve, update or delete a code customer.
-    """
-    try:
-        customer = Customer.objects.get(pk=pk)
-    except Customer.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = CustomerSerializer(customer)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = CustomerSerializer(customer, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        customer.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@csrf_exempt
-@api_view(['GET'])
-def customer_login(request, pin_code):
-    """
-    Retrieve, update or delete a code customer.
-    """
     try:
         customer = Customer.objects.get(pin_code=pin_code)
-    except Customer.DoesNotExist:
+    except (KeyError, Customer.DoesNotExist):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = CustomerSerializer(customer)
+    serializer = CustomerSerializer(customer, data=request.data)
+    if serializer.is_valid():
         return Response(serializer.data)
-
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # elif request.method == 'PUT':
-    #     serializer = CustomerSerializer(customer, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # elif request.method == 'DELETE':
-    #     customer.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
 
 class CustomerViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows customers to be viewed or edited.
     """
-    queryset = Customer.objects.all().order_by('-date_joined')
+
+    queryset = Customer.objects.all().order_by("-date_joined")
     serializer_class = CustomerSerializer
     # disable auth
     # permission_classes = [permissions.IsAuthenticated]
